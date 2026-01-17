b985a1653d623d847da8dfd01884a2e6681265aa
buivi1602 <buivi1602@gmail.com>
2026-01-17 23:11:37 +0700

    creat_headings.html

--- obj/Debug/net10.0/BT PTPMQL.AssemblyInfo.cs
+++ obj/Debug/net10.0/BT PTPMQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT PTPMQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ba7312cb0d7a13fb591d214f4af50347e6502d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af747b6b429b3a123412c69a361b4d871a858ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT PTPMQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT PTPMQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
