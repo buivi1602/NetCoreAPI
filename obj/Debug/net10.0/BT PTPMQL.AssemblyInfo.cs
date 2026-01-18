1710bd2df10b4ee8631987fce43f3eff1f71b6d9
buivi1602 <buivi1602@gmail.com>
2026-01-18 23:33:44 +0700

    Add colgroup section

--- obj/Debug/net10.0/BT PTPMQL.AssemblyInfo.cs
+++ obj/Debug/net10.0/BT PTPMQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT PTPMQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4859fc955238acf304a446b1c879a94b72a82a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2165fa8b260b97934d0b58a37fc1222bf60371c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT PTPMQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT PTPMQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
